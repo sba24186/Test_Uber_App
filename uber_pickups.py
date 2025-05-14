dfc4e7925e2b1ccb962403e87661f0cdc68cac3f
sba24186 <sba24186@student.cct.ie>
2025-05-14 14:57:30 +0100

    Update uber_pickups.py

--- uber_pickups.py
+++ uber_pickups.py
@@ -14,9 +14,13 @@ def load_data(nrows):
     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
     return data
 
-# Create a text element and let the reader know the data is loading.
 data_load_state = st.text('Loading data...')
 # Load 10,000 rows of data into the dataframe.
 data = load_data(10000)
-# Notify the reader that the data was successfully loaded.
+
 data_load_state.text('Loading data...done!')
+
+if st.checkbox('Show raw data'):
+    st.subheader('Raw data')
+    st.write(data)
+    
